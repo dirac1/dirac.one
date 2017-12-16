c5cf434ceb0e7471dba88e3cf46723f1c4ee5d5e
danco1 <dancouno@gmail.com>
2017-12-16 12:52:14 -0400

    gpg and privacy policy

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Daniel Rivero'
 AUTHOR_EMAIL = u'rivero1dan@gmail.com'
-SITENAME = 'dirac is an anagram to..'
-#SITEURL = 'dirac.one'
+SITENAME = 'dirac is an anagram to...'
+SITEURL = 'dirac.one'
 THEME_STATIC_DIR = 'theme'
 THEME = 'theme/voce'
 
@@ -53,22 +53,22 @@ PLUGINS = ['assets','sitemap']
 # Blogroll
 LINKS = (('Home', '/index.html'),
          ('About Me', '/pages/about.html'),
-         ('Résumé', '/pages/cv.html'),)
-
+         ('Résumé', '/pages/cv.html'),
+         ('GPG Key', '/pages/pgp-key.html'),)
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/River0Dan'),
           ('Email', 'mailto:rivero1dan@gmail.com'),)
 
-STATIC_PATHS = ['files/favicon-16x16.png','files/favicon-32x32.png','files/favicon.ico', 'files/CV_DR_ES.pdf','files/CV_DR_EN.pdf','files/banner.png']
-
+STATIC_PATHS = ['files/favicon-16x16.png','files/favicon-32x32.png','files/favicon.ico', 'files/CV_DR_ES.pdf','files/CV_DR_EN.pdf','files/banner.png','/pages/gpg.html']
+ARTICLE_EXCLUDES = ['gpg.html']
 EXTRA_PATH_METADATA = {
     'files/favicon.ico': {'path': 'favicon.ico'},
     'files/favicon-16x16.png': {'path': 'favicon-16x16.png'},
     'files/favicon-32x32.png': {'path': 'favicon-32x32.png'},
     'files/favicon-96x96.png': {'path': 'favicon-96x96.png'},
+#    'files/pgp.html': {'path': 'pages/pgp.html'},
 #    'files/CV_DR_EN.pdf': {'path': 'CV_DR_EN.pdf'},
 #    'files/CV_DR_ES.pdf': {'path': 'CV_DR_ES.pdf'},
-#    'files/banner.png'  : {'path': 'files/banner.png'},
 }
 
 DEFAULT_PAGINATION = False
